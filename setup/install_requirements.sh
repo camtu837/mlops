f29f5c7422dc853d51ace4b21458ef231f585a81
Cẩm Tú <114469155+camtu837@users.noreply.github.com>
2023-12-11 02:18:59 +0700

    Update install_requirements.sh

--- setup/install_requirements.sh
+++ setup/install_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 --version
-pip3 install --upgrade azureml-sdk
-pip3 install -r requirements.txt
-pip3 install --upgrade azure-cli
+pip install --upgrade azureml-sdk
+pip install -r requirements.txt
+pip install --upgrade azure-cli
